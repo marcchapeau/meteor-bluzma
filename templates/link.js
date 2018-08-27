af3e22240c9e9924c13fb3eb440e79f92c0a5d9f
Marc Chapeau <marcchapeau@gmail.com>
2018-08-27 11:04:12 +0200

    Bug fix Link

--- templates/link.js
+++ templates/link.js
@@ -2,6 +2,6 @@ import Bluzma from 'meteor/chap:bluzma/bluzma'
 
 import './link.html'
 
-const bluzmaLink = new Bluzma('Link', ['content', 'href'])
+const bluzmaLink = new Bluzma('Link', ['content'])
 
 bluzmaLink.register()
