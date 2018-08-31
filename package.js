da747bacbff6c9e881ddde5fb3e7aa2eb3daf1d5
Marc Chapeau <marcchapeau@gmail.com>
2018-08-31 13:17:06 +0200

    Add README

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
   name: 'chap:bluzma',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'A set of Blaze components for Bulma',
   git: 'https://github.com/marcchapeau/meteor-bluzma.git',
-  documentation: null
+  documentation: 'README.md'
 })
 
 Package.onUse(function (api) {
