5154559cfeb3d3915a32cfa5315f6a830085c971
Marc Chapeau <marcchapeau@gmail.com>
2018-09-03 10:40:38 +0200

    Version 0.1.8

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'chap:bluzma',
-  version: '0.1.7',
+  version: '0.1.8',
   summary: 'A set of Blaze components for Bulma',
   git: 'https://github.com/marcchapeau/meteor-bluzma.git',
   documentation: 'README.md'
