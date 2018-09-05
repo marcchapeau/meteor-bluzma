d7ebe5dbae61f3c2ab2833eb3270b252635a3271
Marc Chapeau <marcchapeau@gmail.com>
2018-09-05 15:01:19 +0200

    Add Buttons

--- templates/button.js
+++ templates/button.js
@@ -7,7 +7,7 @@ import './button.html'
 
 const bluzmaButton = new Bluzma('Button', [
   'active', 'color', 'content', 'focused', 'fullwidth', 'hovered', 'inverted',
-  'loading', 'outlined', 'rounded', 'size', 'static'
+  'loading', 'outlined', 'rounded', 'selected', 'size', 'static'
 ])
 
 bluzmaButton.helpers({
@@ -23,6 +23,7 @@ bluzmaButton.helpers({
   loading: () => Template.currentData().loading && 'is-loading',
   outlined: () => Template.currentData().outlined && 'is-outlined',
   rounded: () => Template.currentData().rounded && 'is-rounded',
+  selected: () => Template.currentData().selected && 'is-selected',
   size () {
     const size = Template.currentData().size
     return size && `is-${size}`
@@ -32,72 +33,10 @@ bluzmaButton.helpers({
 
 bluzmaButton.register()
 
-// import { Template } from 'meteor/templating'
+const bluzmaButtons = new Bluzma('Buttons', ['addons'])
 
-// import './button.html'
+bluzmaButtons.helpers({
+  addons: () => Template.currentData().addons && 'has-addons'
+})
 
-// import { mapAttributsToClass, mapAttributsToOthers } from '../helpers'
-
-// const attributs = [
-//   'onClick',
-//   'label'
-// ]
-
-// Template.bluzmaButton.helpers({
-//   class () { return mapAttributsToClass(Template.currentData()) },
-//   others () { return mapAttributsToOthers(Template.currentData(), attributs) }
-// })
-
-// // const attributs = {
-// //   active: {class: () => 'is-active'},
-// //   class: {class: classes => classes},
-// //   color: {class: color => `is-${color}`},
-// //   focused: {class: () => 'is-focused'},
-// //   fullwidth: {class: () => 'is-fullwidth'},
-// //   hovered: {class: () => 'is-hovered'},
-// //   inverted: {class: () => 'is-inverted'},
-// //   label: true,
-// //   loading: {class: () => 'is-loading'},
-// //   onClick: true,
-// //   outlined: {class: () => 'is-outlined'},
-// //   rounded: {class: () => 'is-rounded'},
-// //   size: {class: size => `is-${size}`},
-// //   static: {class: () => 'is-static'}
-// // }
-
-// Template.bluzmaButton.onCreated(function () {
-// })
-
-// Template.bluzmaButton.onRendered(function () {
-// })
-
-// // Template.bluzmaButton.helpers({
-// //   class () {
-// //     const data = Template.currentData()
-// //     const list = []
-// //     for (const prop in data) {
-// //       if (attributs[prop] && attributs[prop].class) {
-// //         list.push(attributs[prop].class(data[prop]))
-// //       }
-// //     }
-// //     // console.log('bluzmaButton.helpers.class', list)
-// //     return list.join(' ')
-// //   },
-// //   others () {
-// //     const data = Template.currentData()
-// //     const list = {}
-// //     for (const prop in data) if (!attributs[prop]) list[prop] = data[prop]
-// //     // console.log('bluzmaButton.helpers.others', list)
-// //     if (Object.keys(list).length) return list
-// //   }
-// // })
-
-// Template.bluzmaButton.events({
-//   'click .button' (evt, tpl) {
-//     const onClick = Template.currentData().onClick
-//     if (onClick) onClick(evt, tpl)
-//   }
-// })
-
-// Template.bluzmaButton.onDestroyed(function () {
-// })
+bluzmaButtons.register()
