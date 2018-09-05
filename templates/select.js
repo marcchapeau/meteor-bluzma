774c857dbf90b6fbe5ff812022b7b4d5f7d9e7bc
Vincent Morvan <mvinc@vinc.local>
2018-09-05 13:36:50 +0200

    fix bluzmaSelect and add bluzmaLabel

--- templates/select.js
+++ templates/select.js
@@ -1,22 +1,36 @@
-// import { Template } from 'meteor/templating'
+import { Template } from 'meteor/templating'
 
-// import { mapAttributsToClass, mapAttributsToOthers } from '../helpers'
+import Bluzma from 'meteor/chap:bluzma/bluzma'
 
-// import './select.html'
+import './select.html'
 
-// const attributs = [
-//   'options',
-//   'defaultOption'
-// ]
+const bluzmaSelect = new Bluzma('Select', [
+  'multiple', 'size', 'color', 'rounded',
+  'loading', 'fullwidth', 'expanded', 'iconsLeft',
+  'focused', 'name', 'defaultOption'
+])
 
-// Template.bluzmaSelect.helpers({
-//   class () { return mapAttributsToClass(Template.currentData()) },
-//   others () { return mapAttributsToOthers(Template.currentData(), attributs) }
-// })
+bluzmaSelect.helpers({
+  rounded: () => Template.currentData().rounded && `is-rounded`,
+  iconsLeft: () => Template.currentData().iconLeft && 'has-icons-left',
+  multiple: () => Template.currentData().multiple && 'is-multiple',
+  loading: () => Template.currentData().loading && `is-loading`,
+  fullwidth: () => Template.currentData().fullwidth && `is-fullwidth`,
+  focused: () => Template.currentData().focused && `is-focused`,
+  hovered: () => Template.currentData().hovered && `is-hovered`,
+  color: () => {
+    const color = Template.currentData().color
+    return color && `is-${color}`
+  },
+  size: () => {
+    const size = Template.currentData().size
+    return size && `is-${size}`
+  },
+  expanded: () => Template.currentData().expanded && `is-expanded`
+})
 
-// const optionsAttributs = []
+bluzmaSelect.register()
 
-// Template.bluzmaSelectOption.helpers({
-//   class () { return mapAttributsToClass(Template.currentData()) },
-//   others () { return mapAttributsToOthers(Template.currentData(), optionsAttributs) }
-// })
+const bluzmaSelectOption = new Bluzma('SelectOption', [])
+
+bluzmaSelectOption.register()
